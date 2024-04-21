a2a103502603fbe2dd23b319df07eb41ba40bbc6
Brandon Chung <chung.brandon123@gmail.com>
2024-04-20 19:17:18 -0500

    Add unit tests to file system traversal

---
+++ fstraversal/traversal_test.go
@@ -0,0 +1,124 @@
+package fstraversal
+
+import (
+	"fmt"
+	"io/fs"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type MockDirEntry struct {
+	name string
+}
+
+func (m MockDirEntry) Name() string               { return m.name }
+func (m MockDirEntry) IsDir() bool                { return false }
+func (m MockDirEntry) Type() fs.FileMode          { return 0 }
+func (m MockDirEntry) Info() (fs.FileInfo, error) { return nil, nil }
+
+func TestFsTreeTraversal(t *testing.T) {
+	assert := assert.New(t)
+
+	testData := []struct {
+		expected []string
+		path     string
+	}{
+		{expected: []string{"└── " + dirStyle.Render("b"),
+			"    └── " + dirStyle.Render("c"),
+			"        ├── cc.py",
+			"        └── " + dirStyle.Render("d"),
+			"            ├── a.txt",
+			"            └── b.txt",
+		},
+			path: "../mockDirectory"},
+	}
+
+	for _, d := range testData {
+		t.Run("Test tree traversal", func(t *testing.T) {
+			lines := []string{}
+
+			if err := traverseFsTree(d.path, "", true, false, &lines); err != nil {
+				t.Fatal(err)
+			}
+
+			assert.ElementsMatch(d.expected, lines)
+		})
+	}
+
+	t.Run("Test invalid file path", func(t *testing.T) {
+		path := "./invalidpath"
+		lines := []string{}
+		expected := fmt.Sprintf("error reading directory, open %s: The system cannot find the file specified.", path)
+
+		err := traverseFsTree(path, "", true, false, &lines)
+		assert.EqualError(err, expected)
+	})
+}
+
+func TestTextFormatting(t *testing.T) {
+	assert := assert.New(t)
+
+	testData := []struct {
+		expected  string
+		delimiter string
+		fileName  string
+		isDir     bool
+	}{
+		{expected: "├── file.txt", delimiter: "├── ", fileName: "file.txt", isDir: false},
+		{expected: "│   file.txt", delimiter: "│   ", fileName: "file.txt", isDir: false},
+		{expected: "└── file.txt", delimiter: "└── ", fileName: "file.txt", isDir: false},
+		{expected: "└── " + dirStyle.Render("folder"), delimiter: "└── ", fileName: "folder", isDir: true},
+	}
+
+	for _, d := range testData {
+		t.Run("Test text formatting", func(t *testing.T) {
+			output := formatOutputText(d.delimiter, d.fileName, d.isDir)
+			assert.Equal(d.expected, output, "Expected %s, got %s", d.expected, output)
+		})
+	}
+}
+
+func TestFindLastFileIndex(t *testing.T) {
+	assert := assert.New(t)
+
+	testData := []struct {
+		expected   int
+		files      []fs.DirEntry
+		showHidden bool
+	}{
+		{expected: 2, files: []fs.DirEntry{
+			MockDirEntry{name: "file1.txt"},
+			MockDirEntry{name: ".hiddenfile"},
+			MockDirEntry{name: "file2.txt"},
+		}, showHidden: false},
+		{expected: -1, files: []fs.DirEntry{}, showHidden: false},
+	}
+
+	for _, d := range testData {
+		t.Run("Test finding last index", func(t *testing.T) {
+			output := getLastFileIndex(d.files, d.showHidden)
+			assert.Equal(d.expected, output, "Expected %s, got %b", d.expected, output)
+		})
+	}
+}
+
+func TestHiddenFile(t *testing.T) {
+	assert := assert.New(t)
+
+	testData := []struct {
+		expected bool
+		fileName string
+	}{
+		{expected: false, fileName: "file.txt"},
+		{expected: false, fileName: "__file.txt"},
+		{expected: true, fileName: ".env"},
+	}
+
+	for _, d := range testData {
+		t.Run("Test if hidden file", func(t *testing.T) {
+			output := isHiddenFile(d.fileName)
+			assert.Equal(d.expected, output, "Expected %s, got %b", d.expected, output)
+		})
+	}
+}
--- go.mod
+++ go.mod
@@ -5,17 +5,21 @@ go 1.22.0
 require (
 	github.com/charmbracelet/lipgloss v0.10.0
 	github.com/spf13/cobra v1.8.0
+	github.com/stretchr/testify v1.9.0
 )
 
 require (
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/sys v0.12.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
